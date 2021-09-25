9cc0cd0632e9f079e9aa739717d879561c33914e
eldawidos1111 <eldawidos111@gmail.com>
2021-09-25 15:05:44 +0200

    Bugged zoom

--- Projekt/Game1.cs
+++ Projekt/Game1.cs
@@ -8,7 +8,22 @@ namespace Projekt
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+<<<<<<< Updated upstream
 
+=======
+        float targetX =128  ;
+        float targetY;
+        float zoom = 1;
+        Texture2D province_desert, province_farmland, province_forest, province_jungle, province_lake, province_mountains, province_plains, province_sea, province_taiga, province_tundra,province_coast,province_hills;
+        Vector2 Camera_position = Vector2.Zero;
+        Vector2 scale;
+        string path = "map.txt";
+        string s;
+        string[,] mapaS; 
+        Province[,] mapa;
+        terrain s2;
+        int i = 0,x,y;
+>>>>>>> Stashed changes
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -34,7 +49,37 @@ namespace Projekt
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+<<<<<<< Updated upstream
 
+=======
+            KeyboardState keystate = Keyboard.GetState();
+            if(keystate.IsKeyDown(Keys.W))
+            {
+                Camera_position.Y = Camera_position.Y - (20/zoom);
+            }
+            if (keystate.IsKeyDown(Keys.S))
+            {
+                Camera_position.Y = Camera_position.Y + (20/zoom);
+            }
+            if (keystate.IsKeyDown(Keys.A))
+            {
+                Camera_position.X = Camera_position.X -(20/zoom);
+            }
+            if (keystate.IsKeyDown(Keys.D))
+            {
+                Camera_position.X = Camera_position.X + (20/zoom);
+            }
+            if (keystate.IsKeyDown(Keys.Up)&&zoom>0.5   )
+            {
+                Camera_position = Camera_position * 1.01f;
+                zoom = zoom - (float)0.01;
+            }
+            if (keystate.IsKeyDown(Keys.Down)&&zoom<8)
+            {
+                Camera_position = Camera_position * 0.99f;
+                zoom = zoom + (float)0.01;
+            }
+>>>>>>> Stashed changes
             // TODO: Add your update logic here
 
             base.Update(gameTime);
