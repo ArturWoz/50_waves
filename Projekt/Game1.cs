f279824c2597171deee0f8760cbc0ee6470c095a
eldawidos1111 <eldawidos111@gmail.com>
2021-09-25 15:10:26 +0200

    need help with zoom to make it smooth

--- Projekt/Game1.cs
+++ Projekt/Game1.cs
@@ -83,26 +83,28 @@ namespace Projekt
             KeyboardState keystate = Keyboard.GetState();
             if(keystate.IsKeyDown(Keys.W))
             {
-                Camera_position.Y = Camera_position.Y - 5;
+                Camera_position.Y = Camera_position.Y - (20/zoom);
             }
             if (keystate.IsKeyDown(Keys.S))
             {
-                Camera_position.Y = Camera_position.Y + 5;
+                Camera_position.Y = Camera_position.Y + (20/zoom);
             }
             if (keystate.IsKeyDown(Keys.A))
             {
-                Camera_position.X = Camera_position.X -5;
+                Camera_position.X = Camera_position.X -(20/zoom);
             }
             if (keystate.IsKeyDown(Keys.D))
             {
-                Camera_position.X = Camera_position.X + 5;
+                Camera_position.X = Camera_position.X + (20/zoom);
             }
             if (keystate.IsKeyDown(Keys.Up)&&zoom>0.5)
             {
+                Camera_position = Camera_position / 0.99f;
                 zoom = zoom - (float)0.01;
             }
             if (keystate.IsKeyDown(Keys.Down)&&zoom<8)
             {
+                Camera_position = Camera_position / 1.01f;
                 zoom = zoom + (float)0.01;
             }
             // TODO: Add your update logic here
