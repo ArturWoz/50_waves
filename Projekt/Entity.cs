be689434925963622cf9a4692b082a3ae650a438
eldawidos1111 <eldawidos111@gmail.com>
2021-09-29 15:07:19 +0200

    Renamed Class 1 to FrameCounter, added Entity class
    Entity class is base class to all type of units any nation can wield.

---
+++ Projekt/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt
+{
+    public enum stance
+    {
+        idle, moving, fighting
+    }
+    public enum status
+    {
+        dead, alive
+    }
+    abstract class Entity
+    {
+        Province position;
+        stance stance = stance.idle;
+        status status = status.alive;
+        int hp;
+        byte melee_defence;
+        byte melee_attack;
+        byte ranged_defence;
+        byte attack_damage;
+        byte movement_points;
+        byte owner_id;
+        int cost;
+        void move(Province position) 
+        {
+            if (movement_points >= position.GetProvince_movement()) this.position = position; stance = stance.moving;
+        }
+        void kill(Nation killer) { position = null;stance = stance.idle;status = status.dead;hp = 0;melee_defence = 0;melee_attack = 0;ranged_defence = 0;movement_points = 0; if (killer.GetID() != owner_id) killer.AddSlaves(cost/2); }
+        int GetCost() { return this.cost; }
+        void cancel_stance() { this.stance = stance.idle; }
+    }
+}
