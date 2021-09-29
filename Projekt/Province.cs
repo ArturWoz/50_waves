3d2e97327b4b179932712105cd725ecbda825314
eldawidos1111 <eldawidos111@gmail.com>
2021-09-29 18:10:32 +0200

    Update Province.cs

--- Projekt/Province.cs
+++ Projekt/Province.cs
@@ -39,7 +39,7 @@ namespace Projekt
         public int GetID() { return this.province_id; }
 
         public bool GetClicked() { return this.clicked; } //LMAO get clicked n00b
-        public byte GetProvince_movement() { return this.province_movement; }
+        public int GetProvince_movement() { return this.province_movement; }
 
         public void SetClicked(bool value) { this.clicked = value; }
     }
