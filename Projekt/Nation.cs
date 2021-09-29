802cc50cdf17c6bd79647de67e64e1d252ae7414
eldawidos1111 <eldawidos111@gmail.com>
2021-09-29 15:08:07 +0200

    Update Nation.cs

--- Projekt/Nation.cs
+++ Projekt/Nation.cs
@@ -6,22 +6,30 @@ namespace Projekt
 {
     public class Nation
     {
-        int nation_id;
+        byte nation_id;
+        int slaves=0;
         string nation_name;
         bool is_player_led;
         double nation_gold;
 
-        public Nation(int nation_id, string nation_name, bool is_player_led, double nation_gold)
+        public Nation(byte nation_id, string nation_name, bool is_player_led, double nation_gold)
         {
             this.nation_id = nation_id;
             this.nation_name = nation_name;
             this.is_player_led = is_player_led;
             this.nation_gold = nation_gold;
         }
-        public void SetGold(double nation_gold)
+        public void AddGold(double nation_gold)
         {
-            if (nation_gold < 0) throw new Exception("[REDACTED]");
-            this.nation_gold = nation_gold;
+            this.nation_gold +=nation_gold;
+        }
+        public void AddSlaves(int slaves)
+        {
+            this.slaves += slaves;
+        }
+        public byte GetID()
+        {
+            return nation_id;
         }
     }
 }
